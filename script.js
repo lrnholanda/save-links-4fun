3fab127fc891797fdc785813b88b47078e1ae141
Lorena Holanda <lrnholanda@gmail.com>
2023-04-12 20:51:08 -0300

    updated script.js

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
+let myList = [];
 const saveBtn = document.getElementById('save');
 const dltBtn = document.getElementById('delete');
 const inputLink = document.querySelector('input');
-const linklist =document.getElementsByClassName('list');
+const linklist = document.getElementById('list');
+
 
 saveBtn.addEventListener('click', function(){
-    const link = inputLink.value;
-    console.log(link);
+    const link = inputLink.value
+    myList.push(link);
+    inputLink.value = '';
+    localStorage.setItem('list', JSON.stringify(myList));
+    render(myList);
+    alert('saved! 🎉')
 });
 
 dltBtn.addEventListener('click', function(){
-    console.log('deleted 😝');
+    localStorage.clear()
+    myList = []
+    render(myList)
 });
+
+function render(links) {
+ let list = ''
+ for (let i = 0; i < links.length; i++) {
+    list += `
+      <li>
+           <a  target="_blank" href="${links[i]}">${links[i]}</a>
+      </li>
+    `
+ }
+ linklist.innerHTML = list
+}
